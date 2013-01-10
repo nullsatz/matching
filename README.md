53435ce8dd8b4da4459e3e5743094a882ba4e3ec
Josh Buckner <nullsatz@gmail.com>
2013-01-10 16:19:37 -0500

    correcting git copy mistake

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 matching
 ========
 
 a place to test matching strategies outside of any other application or contraints
-=======
-gpu.match
-=========
-
-cuda-ized version of the auction algo
->>>>>>> ad9ae087b1190b1656ef6f0a27cb8a9f74db0eed
---
+++ match.R
@@ -0,0 +1,98 @@
+#!/usr/bin/Rscript
+
+trial_match <- function(nBidders, nItems) {
+	trial <- data.frame(bidder=character(nBidders), item=character(nItems),
+		benefit=runif(nBidders * nItems), stringsAsFactors=FALSE)
+	trial$bidder <- as.character(sapply(1:nBidders, rep, times=nItems))
+	trial$items <- rep(as.character(1:nItems), nBidders)
+	return(trial)
+}
+
+bid_phase <- function(benefits, prices) {
+	bidders <- unique(benefits$bidder)
+	bid_list <- list()
+	nbids <- 0
+	for(current_bidder in bidders) {
+		bidder_benefits <- subset(benefits, benefits$bidder == current_bidder)
+
+		values <- merge(bidder_benefits, prices, by='item')
+		values$value <- values$benefit - values$price
+		values <- values[order(values$value, decreasing=TRUE), ]
+
+		if(nrow(values) < 1)
+			next
+
+		max1_value <- values[1, 'value']
+		if(is.na(max1_value))
+			next
+		max_item <- values[1, 'item']
+
+		if(nrow(values) < 2) {
+			max2_value <- 0.0
+		} else {
+			max2_value <- values[2, 'value']
+			if(is.na(max2_value))
+				max2_value <- 0.0
+		}
+		nbids <- nbids + 1
+		bid_list[[nbids]] <- data.frame(bidder=current_bidder, item=max_item,
+			bid = max1_value - max2_value + 1.0, stringsAsFactors=FALSE)
+	}
+	if(nbids > 0) {
+		bids <- do.call('rbind', bid_list)
+	} else {
+		bids <- data.frame(bidder=character(0), item=character(0),
+			bid=numeric(0), stringsAsFactors=FALSE)
+	}
+	return(bids)
+}
+
+assignment_phase <- function(bids, prices, assignments) {
+	items <- unique(bids$item)
+	for(current_item in items) {
+		item_bids <- subset(bids, !is.na(bids$bid) & bids$item == current_item)
+
+		if(nrow(item_bids) < 1)
+			next
+
+		item_bids <- item_bids[order(item_bids$bid, decreasing=TRUE), ]
+		max_bid <- item_bids[1, 'bid']
+		max_bidder <- item_bids[1, 'bidder']
+
+		prices$price <- ifelse(prices$item == current_item,
+			prices$price + max_bid, prices$price)
+		assignments$assigned <- ifelse(assignments$item == current_item &
+			assignments$bidder == max_bidder, TRUE, assignments$assigned)
+		assignments$assigned <- ifelse(assignments$item == current_item &
+			assignments$bidder != max_bidder, FALSE, assignments$assigned)
+	}
+	round_results <-list()
+	round_results[['prices']] <- prices
+	round_results[['assignments']] <- assignments
+	return(round_results)
+}
+
+unmatched <- function(assignments) {
+	items <- unique(assignments$item)
+	for(current_item in items) {
+		matches <- subset(assignments, assignments$item == current_item)
+		if(!any(matches$assigned)) {
+			return(TRUE)
+		}
+	}
+	return(FALSE)
+}
+
+auction <- function(benefits) {
+	prices <- data.frame(item=unique(benefits$item), price=0, 
+	    stringsAsFactors=FALSE)
+	assignments <- data.frame(bidder=benefits$bidder, item=benefits$item,
+		assigned=FALSE, stringsAsFactors=FALSE)
+	while(unmatched(assignments)) {
+		bids <- bid_phase(benefits, prices)
+		new_info <- assignment_phase(bids, prices, assignments)
+		prices <- new_info$prices
+		assignments <- new_info$assignments
+	}
+	return(assignments)
+}
